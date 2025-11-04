05eb3e2fa9347ca3555941e43e4be53b9fa0aa10
soroush <soroushfalaki24@hotmail.com>
2025-11-04 19:09:15 +0330

    add hash password

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,15 +1,23 @@
 import { UserModel } from '../models/usermodle.js';
+import bcrypt, { hash } from 'bcryptjs';
 
 export const registerUserCtrl = async (req, res) => {
   try {
     const { username, email, password } = req.body;
-    const userExist = UserModel.findByEmail(email);
+    const userExist = await UserModel.findByEmail(email);
     if (userExist) {
       return res
         .status(400)
         .json({ status: 'fail', msg: 'user already exists' });
     }
-    const user = await UserModel.create({ username, email, password });
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await UserModel.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
     res.status(201).json({
       status: 'success',
       msg: 'user registered successfully',
@@ -23,15 +31,16 @@ export const registerUserCtrl = async (req, res) => {
 export const loginUserCtrl = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const userFound = UserModel.findByEmail(email);
-    if (!userFound) {
+    const userFound = await UserModel.findByEmail(email);
+    if (userFound && (await bcrypt.compare(password, userFound?.password))) {
+      res.json({
+        status: 'success',
+        msg: 'login successful',
+        data: userFound,
+      });
+    } else {
       return res.status(404).json({ status: 'fail', msg: 'user not found' });
     }
-    res.json({
-      status: 'success',
-      msg: 'login successful',
-      data: userFound,
-    });
   } catch (err) {
     res.status(500).json({ status: 'error', msg: err.message });
   }
