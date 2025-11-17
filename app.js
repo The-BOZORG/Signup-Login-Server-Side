3529684e0dc60d7bfba1335141403310c818772b
soroush <soroushfalaki24@hotmail.com>
2025-11-17 12:13:23 +0330

    add middleware

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import path from 'path';
 import userRoutes from './routes/userroutes.js';
+import logger from './middleware/logger.js';
 
 const app = express();
 app.use(express.json());
 app.use(express.static(path.join(path.resolve(), 'src')));
 
+app.use(logger);
 app.use('/', userRoutes);
 
 export default app;
